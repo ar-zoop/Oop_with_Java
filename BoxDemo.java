110ab9c88f34d1d3df76baaef7582d5856f67789
arzoop <arzoobapna01@gmail.com>
2022-07-23 20:51:47 +0530

    Overloading

--- BoxDemo.java
+++ BoxDemo.java
@@ -1,22 +1,43 @@
 //Chapter 6
-//Introducing Classes
+//doubleroducing Classes
 import java.util.* ;
 class Box{
     double width;
     double height;
     double depth;
+    
+    Box(){
+        width=-1;
+        height=-1;
+        depth=-1;
+    }
+    Box(double w, double h, double d){
+        width=w;
+        height=h;
+        depth=d;
+    }
+    Box(double l){
+        width=l;
+        height=l;
+        depth=l;   
+    }
+    double volume(){
+        return width*height*depth;
+    }
 };
 
 class BoxDemo{
     public static void main(String args[]){
-        Box mybox=new Box();
+        Box mybox1=new Box();
+        Box mybox2=new Box(1.5, 2, 3);
+        Box mybox3=new Box(5);
         double vol;
-        mybox.width=10;
-        mybox.height=20;
-        mybox.depth=30;
-        vol=mybox.width* mybox.height * mybox.depth;
-        System.out.println(vol);w
-        Box mybox2=new Box();
+        vol=mybox1.volume();
+        System.out.println(vol);
+        vol=mybox2.volume();
+        System.out.println(vol);
+        vol=mybox3.volume();
+        System.out.println(vol);
     }
 };
 
--- readme.md
+++ readme.md
@@ -121,5 +121,46 @@ class HelloWorldd {
 
 ### 6.	WAP to display the concept of method overloading.
 ```
+import java.util.* ;
+class Box{
+    double width;
+    double height;
+    double depth;
+    
+    Box(){
+        width=-1;
+        height=-1;
+        depth=-1;
+    }
+    Box(double w, double h, double d){
+        width=w;
+        height=h;
+        depth=d;
+    }
+    Box(double l){
+        width=l;
+        height=l;
+        depth=l;   
+    }
+    double volume(){
+        return width*height*depth;
+    }
+};
+
+class BoxDemo{
+    public static void main(String args[]){
+        Box mybox1=new Box();
+        Box mybox2=new Box(1.5, 2, 3);
+        Box mybox3=new Box(5);
+        double vol;
+        vol=mybox1.volume();
+        System.out.println(vol);
+        vol=mybox2.volume();
+        System.out.println(vol);
+        vol=mybox3.volume();
+        System.out.println(vol);
+    }
+};
+
 
 ```
